d03e49c3bcc42cf97cf909d7aee2a4b6ba923de3
Nikhil <work.nikhilgupta5@gmail.com>
2026-02-02 10:38:41 +0530

    Challenge: Variables & Data Types

---
+++ Day 2 Challenge Variables & Data Types/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Alice"
+	age := 30
+	var height float64 = 5.7
+	var isStudent bool = false
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Is Student:", isStudent)
+}
